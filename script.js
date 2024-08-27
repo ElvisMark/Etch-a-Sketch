68df7212d638d946663bf435601c6ab04c5004fe
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-08-27 15:27:13 +0300

    created a function to create the square container

---
+++ script.js
@@ -0,0 +1,10 @@
+function createGrid() {
+    const container = document.querySelector('#square-container')
+
+
+    for(let i = 0; i < 256; i++){
+        const square = document.createElement('div')
+        square.className = 'square';
+        container.appendChild(square)
+    }
+}
