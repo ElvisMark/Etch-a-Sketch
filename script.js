6c4e6469aad5149a66803775a1485db94928e09d
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-08-28 08:13:46 +0300

    restractured the code to do new task, added prompt and button

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-function createGrid() {
+function createGrid(squaresPerSide) {
     const container = document.querySelector('#square-container')
 
+    container.innerHTML= '';
 
-    for(let i = 0; i < 256; i++){
+    const squareSize =960 / squaresPerSide;
+
+    container.style.gridTemplateColumns =`repeat(${squaresPerSide}, ${squareSize}px)`;
+    container.style.gridTemplateRows = `repeat(${squaresPerSide}, ${squareSize}px)`
+
+
+    for(let i = 0; i < squaresPerSide * squaresPerSide; i++){
         const square = document.createElement('div')
         square.className = 'square';
+        square.style.width = `${squareSize}px`
+        square.style.height = `${squareSize}px`
+
 
         square.addEventListener('mouseover', function(){
             square.style.backgroundColor = '#073f3f'
         })
 
-        // square.addEventListener('mouseout', function(){
-        //     square.style.backgroundColor = '#ab48c9'
-        // })
-
-
         container.appendChild(square)
     }
 }
 
+function gridResize () {
+    let squaresPerSide = parseInt(prompt('Enter the number of squares per side between 1 and 100:'),10);
+
+    if(squaresPerSide > 0 && squaresPerSide < 100) {
+        createGrid(squaresPerSide);
+    }else {
+        alert('Enter a number between 1 to 100');
+    }
+}
+
 window.onload = createGrid;
+createGrid(16);
+
+document.getElementById('prompt-btn').addEventListener('click', gridResize)
