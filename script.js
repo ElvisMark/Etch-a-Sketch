37032dc36e3082564bf29772cb950b90550d269e
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-08-28 06:59:47 +0300

    added the pixeleted effect

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function createGrid() {
     for(let i = 0; i < 256; i++){
         const square = document.createElement('div')
         square.className = 'square';
+
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = '#073f3f'
+        })
+
+        // square.addEventListener('mouseout', function(){
+        //     square.style.backgroundColor = '#ab48c9'
+        // })
+
+
         container.appendChild(square)
     }
 }
